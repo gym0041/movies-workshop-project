f54be3d3d80364b71f8baf74dc89482de4565657
Plamen Buchkov <gym0041@gmail.com>
2025-01-26 13:05:16 -0300

    adding image URLs

---
+++ src/movies.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        id: 1,
+        name: "The Shawshank Redemption",
+        genre: "Prision Drama",
+        description: "A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.",
+        imageUrl: "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_.jpg"
+    },
+    {
+        id: 2,
+        name: "The Godfather",
+        genre: "Gangster",
+        description: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8F9hpABISwNl4sXh8a2lpL-EoQ7EUkDYicQ&s"
+    },
+    {
+        id: 3,
+        name: "The Dark Knight",
+        genre: "Action Epic",
+        description: "When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness.",
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpvyijfsP4F8o9bqAZc6bxCdbAYeKc582P1A&s"
+    }
+]
