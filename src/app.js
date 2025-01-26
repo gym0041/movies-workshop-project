07108bef5488b40d5799200ff608aa1f2c44af64
Plamen Buchkov <gym0041@gmail.com>
2025-01-26 11:40:49 -0300

    Creating the server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Welcome from the Home Page")
+})
+
+app.listen(5000, "localhost", () => {
+    console.log("Server started. Listening on http://localhost:5000...")
+})
